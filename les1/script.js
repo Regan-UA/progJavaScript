e4c8495c9d48db6a37f8813a93f0b43a1f45174b
ReganUA <162838412+ReganUA@users.noreply.github.com>
2025-11-07 09:35:02 +0100

    java

---
+++ les1/script.js
@@ -0,0 +1,15 @@
+const huidigeDag = "maandag";
+
+if (
+  huidigeDag == "maandag" ||
+  huidigeDag == "dinsdag" ||
+  huidigeDag == "woensdag" ||
+  huidigeDag == "donderdag" ||
+  huidigeDag == "vrijdag"
+) {
+  console.log("Ik moet naar school");
+} else if (huidigeDag == "zaterdag" || huidigeDag == "zondag") {
+  console.log("Ik ben vandaag vrij");
+} else {
+  console.log("Ongeldige dag ingevoerd");
+}
---
+++ les6/opdracht1/java.js
@@ -0,0 +1,5 @@
+for (let i = 2; i <= 50; i++){
+    for(let j = 2; j <= 50; j++){
+        console.log(i + "*" + j + "=" + i*j);
+    }
+}
